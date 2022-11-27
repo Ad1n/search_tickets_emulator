3f7d2e436c98aab318bf74714b7303b99070b983
Ad1n0d1n <shevtsovav@bk.ru>
2022-11-27 12:07:05 +0100

    add success to batch response struct

--- src/batch_response.rs
+++ src/batch_response.rs
@@ -3,10 +3,11 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct BatchResponse {
     count: u32,
+    status: String
 }
 
 impl BatchResponse {
     pub fn new(count: u32) -> BatchResponse {
-        BatchResponse { count }
+        BatchResponse { count, status: String::from("success") }
     }
 }
